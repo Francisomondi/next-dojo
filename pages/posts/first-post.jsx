09ce09aa25e0368bc3708494f2d1ec33353e4ec2
francis omondi <francisomondi17@gmail.com>
2023-05-02 06:09:31 +0300

    working with assets-images

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import myImage from "../../public/images/a.jpg"
+import Image from 'next/image'
 
 const firstPost = () => {
   return (
     <div>
-    <h1>This is the first post</h1></div>
+     <h1>This is the first post</h1>
+
+     <Image src={myImage}></Image>
+    </div>
   )
 }
 
