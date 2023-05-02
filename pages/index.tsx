6c5f72f0985639d482a3eca0d705446b11076af8
francis omondi <francisomondi17@gmail.com>
2023-05-02 05:43:09 +0300

    linking pages and the head section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-     <h1>Hello My beautiful gallery</h1>
-    </main>
+    <>
+      <Head>
+        <title>My react dojo</title>
+      </Head>
+      <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+          <h1>Hello My beautiful gallery</h1>
+
+          <Link href={'/posts/first-post'}>
+            Go to first post
+          </Link>
+        </main>
+      
+      </>
   )
 }
---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const firstPost = () => {
+  return (
+    <div>
+    <h1>This is the first post</h1></div>
+  )
+}
+
+export default firstPost
